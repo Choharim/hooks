81a71e5428d29ebecedd31749eac572c1cccb0f9
Choharim <jhl8586@naver.com>
2020-12-07 17:34:10 +0900

    Modify useStateHook to  classComponent

--- src/App.js
+++ src/App.js
@@ -19,4 +19,38 @@ const App = () => {
   );
 };
 
-export default App;
+class AppUgly extends React.Component{
+  state = {
+    item:1
+  };
+
+  incrementItem = () => {
+    this.setState(pre => { 
+      return (
+        { item : pre.item + 1}
+      );
+    });
+  }
+  decrementItem = () => {
+    this.setState(pre => {
+      return (
+        { item : pre.item -1}
+      );
+    });
+  }
+
+  render(){
+    const { item } = this.state;
+    return (
+      <div className="AppUgly">
+        <h1>{item}</h1>
+        <button onClick={this.incrementItem}>increment2</button>
+        <button onClick={this.decrementItem}>decrement2</button>
+      </div>
+    );
+  }
+}
+
+
+
+export default AppUgly;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppUgly from './App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<AppUgly />,document.getElementById('root'));
 
