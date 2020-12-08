d2b90554976c34656c611e31d7988216ffcdde85
Choharim <jhl8586@naver.com>
2020-12-08 18:56:46 +0900

    Add useFadeIn

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-const useBeforeLeave  = (onBefore) => {
-  const handle = (event) => {
-    const {clientY} = event;
-    if(clientY <= 0){
-      onBefore();
-    }
-  };
 
+const useFadeIn = (duration = 1,delay = 0) => {
+  const element = useRef();
   useEffect(() => {
-    document.addEventListener("mouseleave",handle);
+    if(element.current){
+      const {current} = element;
+      current.style.opacity = 1;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+    }
 
-    return ( () => {document.removeEventListener("mouseleave",handle);}
-    );
   },[]);
 
-  if(typeof onBefore !== "function"){
+  if(typeof duration !== "number" || typeof delay !== "number"){
     return;
   }
+
+  return {ref: element, style: {opacity:0}};
 };
 
 const App = () => {
-  const begForLife = () => {console.log("please don't leave");};
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(1,2);
+  const fadeInP = useFadeIn(5,5);
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>lorem alalalallaaa</p>
     </div>
   );
 };
