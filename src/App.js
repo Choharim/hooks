c182fabed21ba5f210709aaab4b142fcceb77ef9
Choharim <jhl8586@naver.com>
2020-12-08 21:31:52 +0900

    Add useFullscreen

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-
-const useScroll = () => {
-  const [state ,setState] = useState({x:0,y:0});
-
-  const onScroll = () => {
-    setState({x:window.scrollX, y:window.scrollY});
+const useFullscreen = () => {
+  const element = useRef();
+  const triggerFull = () => {
+    if(element.current){
+      element.current.requestFullscreen();  
+    }
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll",onScroll);
+  const exitFull = () => {
+    document.exitFullscreen();
+  };
 
-    return (() => {
-      window.removeEventListener("scroll",onScroll);
-    });
-  },[]);
-
-  return state;
+  return {element,triggerFull,exitFull};
 };
 
 const App = () => {
-  const {y} = useScroll();
-
+  const {element,triggerFull,exitFull} = useFullscreen();
+ 
   return (
     <div className="App" style={{height: "1000vh"}}>
-     <h1 style={{position:"fixed", color: y > 100? "red" : "blue"}}>Hi</h1>
+      <div ref={element} >
+        <img src="https://lh3.googleusercontent.com/proxy/9MdSBC11pW1bbngwIO7EcNsmE2bqzbQwAac0OWI42PNgy1IO0Q1cTkjyiHSYGFXu9L-v9xKTH-X9jJb92r2KNFyE6Ha1ZGf1H3ezLJVr3_-5Ekp-807DqlxUIt0nAKMcnq4bG5xdfiRYeNTPPuUoNi4JPeMJjWrA5B6s"></img>
+        <button onClick={exitFull}>exitscreen</button>
+      </div>
+      <button onClick={triggerFull}>fullscreen</button>
     </div>
   );
 };
