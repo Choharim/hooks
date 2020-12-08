96e36d2344979b3f8fec3e8fbfe54693ca5f1b3a
Choharim <jhl8586@naver.com>
2020-12-08 14:58:42 +0900

    Add return Fn in useEffect

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ const useClick = (onClick) => {
     if(element.current){
       element.current.addEventListener("click",onClick);
     }
-  });
+    return (() => {
+      if(element.current){
+      element.current.removeEventListener("click",onClick);
+      }
+    });//useEffect에서 function을 return하면 componentWiilUnMount일때 그 function 실행
+
+  },[]);
+
+  if(typeof onClick !== "function"){
+    return;
+  }
+
   return element;
-};
+  };
 
 const App = () => {
   const sayHello = () => console.log("Hello");
