c2c752abb81cb93e71d3f7359fd65104dba50b4f
Choharim <jhl8586@naver.com>
2020-12-08 14:09:59 +0900

    Add reference

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const useTitle = (initTitle) => {
-  const [title, setTitle] = useState(initTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  }
-  useEffect(updateTitle,[title]);
-  return setTitle;
-}
 
 const App = () => {
-  const titleUadate = useTitle("Loading...");
-  setTimeout(() => { titleUadate("Home")}, 5000);
-
+  const potato = useRef();
+  setTimeout(() => {
+    potato.current.focus();
+  }, 5000);
   return (
     <div className="App">
-      
+      <input ref={potato} placeholder="la"></input>
     </div>
   );
 };
